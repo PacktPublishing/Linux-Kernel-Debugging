61357cf2dbf1bb0b59ca8731623ca57f1fd1a116
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2021-09-25 16:30:35 +0530

    1_kprobe: ic

---
+++ ch5/kprobes/1_kprobe/1_kprobe.c
@@ -0,0 +1,108 @@
+/*
+ * ch5/kprobes/1_kprobe/1_kprobe.c
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Linux Kernel Debugging"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *  GitHub repository:
+ *  https://github.com/PacktPublishing/Linux-Kernel-Debugging
+ *
+ * From: Ch 5: Debug via Instrumentation - printk and friends
+ ****************************************************************
+ * Brief Description:
+ * Traditional and manual approach, simplest case: attaching a kprobe,
+ * hard-coding it (to the open system call).
+ *
+ * For details, please refer the book, Ch 5.
+ */
+#define pr_fmt(fmt) "%s:%s(): " fmt, KBUILD_MODNAME, __func__
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/interrupt.h>
+#include <linux/spinlock.h>
+#include <linux/kprobes.h>
+#include <linux/uaccess.h>
+#include "../../../convenient.h"
+
+MODULE_AUTHOR("<insert your name here>");
+MODULE_DESCRIPTION("LKD book:ch5/kprobes/1_kprobe_lkm: simple Kprobes demo module");
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_VERSION("0.1");
+
+static spinlock_t lock;
+static struct kprobe kpb;
+static volatile u64 tm_start, tm_end;
+
+static int verbose;
+module_param(verbose, int, 0644);
+MODULE_PARM_DESC(verbose, "Set to 1 to get verbose printk's (defaults to 0).");
+
+/*
+ * This probe runs just prior to the function "do_sys_open()" is invoked.
+ */
+static int handler_pre(struct kprobe *p, struct pt_regs *regs)
+{
+	PRINT_CTX();	// uses pr_debug()
+
+	spin_lock(&lock);
+	tm_start = ktime_get_real_ns();
+	spin_unlock(&lock);
+
+	return 0;
+}
+
+/*
+ * This probe runs immediately after the function "do_sys_open()" completes.
+ */
+static void handler_post(struct kprobe *p, struct pt_regs *regs, unsigned long flags)
+{
+	spin_lock(&lock);
+	tm_end = ktime_get_real_ns();
+	PRINT_CTX();	// uses pr_debug()
+	SHOW_DELTA(tm_end, tm_start);
+	spin_unlock(&lock);
+	pr_debug("\n"); // silly- just to see the output clearly via dmesg/journalctl
+}
+
+/*
+ * fault_handler: this is called if an exception is generated for any
+ * instruction within the pre- or post-handler, or when Kprobes
+ * single-steps the probed instruction.
+ */
+static int handler_fault(struct kprobe *p, struct pt_regs *regs, int trapnr)
+{
+	pr_info("fault_handler: p->addr = 0x%p, trap #%dn", p->addr, trapnr);
+	/* Return 0 because we don't handle the fault. */
+	return 0;
+}
+NOKPROBE_SYMBOL(handler_fault);
+
+static int __init kprobe_lkm_init(void)
+{
+	/* Register the kprobe handler */
+	kpb.pre_handler = handler_pre;
+	kpb.post_handler = handler_post;
+	kpb.fault_handler = handler_fault;
+	kpb.symbol_name = "do_sys_open";
+	if (register_kprobe(&kpb)) {
+		pr_alert("register_kprobe on do_sys_open() failed!\n");
+		return -EINVAL;
+	}
+	pr_info("registering kernel probe @ 'do_sys_open()'\n");
+	spin_lock_init(&lock);
+
+	return 0;		/* success */
+}
+
+static void __exit kprobe_lkm_exit(void)
+{
+	unregister_kprobe(&kpb);
+	pr_info("bye, unregistering kernel probe @ 'do_sys_open()'\n");
+}
+
+module_init(kprobe_lkm_init);
+module_exit(kprobe_lkm_exit);
---
+++ ch5/kprobes/1_kprobe/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# test.sh
+KMOD=1_kprobe
+VERBOSE=1
+
+DYNDBG_CTRL=/sys/kernel/debug/dynamic_debug/control
+if [ ! -f ${DYNDBG_CTRL} ]; then
+   [ -f /proc/dynamic_debug/control ] && DYNDBG_CTRL=/proc/dynamic_debug/control \
+       || DYNDBG_CTRL=""
+fi
+
+echo "Module ${KMOD}: function to probe: do_sys_open()
+"
+if [ ! -f ./${KMOD}.ko ]; then
+  echo "Building ${KMOD} ..."
+  make || exit 1
+fi
+sudo rmmod ${KMOD} 2>/dev/null # rm any stale instance
+sudo insmod ./${KMOD}.ko verbose=${VERBOSE} || exit 1
+
+[ -z "${DYNDBG_CTRL}" ] && {
+   echo "No dynamic debug control file available..."
+   exit
+}
+
+echo "-- Module ${KMOD} now inserted, turn on any dynamic debug prints now --"
+sudo bash -c "grep \"${KMOD} .* =_ \" ${DYNDBG_CTRL}" && echo "Wrt module ${KMOD}, one or more dynamic debug prints are Off" || \
+ echo "Wrt module ${KMOD}, one or more dynamic debug prints are On"
+# turn On debug prints
+sudo bash -c "echo -n \"module ${KMOD} +p\" > ${DYNDBG_CTRL}"
+sudo bash -c "grep \"${KMOD}\" ${DYNDBG_CTRL}"
+echo "--   All set, look up kernel log with, f.e., journalctl -k -f   --"
+
