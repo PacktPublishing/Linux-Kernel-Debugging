b0adf8944ba571ae19362741ec8565264756eb77
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2021-12-12 16:48:50 +0530

    initial commit, oops v1

---
+++ ch8/oops_tryv1/oops_tryv1.c
@@ -0,0 +1,45 @@
+/*
+ * ch7/oops_tryv1/oops_tryv1.c
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Linux Kernel Debugging"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *  GitHub repository:
+ *  https://github.com/PacktPublishing/Linux-Kernel-Debugging
+ *
+ * From: Ch 7: Oops! Interpreting the kernel bug diagnostic
+ ****************************************************************
+ * Brief Description:
+ *
+ * For details, please refer the book, Ch 8.
+ */
+#define pr_fmt(fmt) "%s:%s():%d: " fmt, KBUILD_MODNAME, __func__, __LINE__
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/random.h>
+
+MODULE_AUTHOR("<insert your name here>");
+MODULE_DESCRIPTION("LKD book:ch7/oops_tryv1: generates a kernel Oops! a kernel bug");
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_VERSION("0.1");
+
+static int __init try_oops_init(void)
+{
+	u8 val = 0x0;
+
+	pr_info("Lets Oops!\nNow reading content at the NULL address 0x%p\n", NULL);
+	val = *(int *)0x0;
+
+	return 0;		/* success */
+}
+
+static void __exit try_oops_exit(void)
+{
+	pr_info("Goodbye, from Oops try\n");
+}
+
+module_init(try_oops_init);
+module_exit(try_oops_exit);
