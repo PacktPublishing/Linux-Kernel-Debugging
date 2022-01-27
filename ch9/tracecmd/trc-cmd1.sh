5f5d00c24e61e4a5175d944d0d0840d80940d74b
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2022-01-27 08:18:12 +0530

    Add the runcmd()

--- ch9/tracecmd/trc-cmd1.sh
+++ ch9/tracecmd/trc-cmd1.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # ch9/trace-cmd/trc-cmd1.sh
 # Simple wrapper over trace-cmd
-
 [ $# -ne 1 ] && {
   echo "Usage: $0 report-file"
   exit 1
 }
+runcmd()
+{
+    [ $# -eq 0 ] && return
+    echo "$@"
+    eval "$@"
+}
 rep=$1
 
 #--- RECORD
-echo "sudo trace-cmd record -p function_graph -e net -e sock -F ping -c1 packtpub.com"
-sudo trace-cmd record -p function_graph -e net -e sock -F ping -c1 packtpub.com
+runcmd sudo trace-cmd record -p function_graph -e net -e sock -F ping -c1 packtpub.com
 
 #--- REPORT
 [ -f $1 ] && mv -f $1 $1.bkp
-echo "sudo trace-cmd report -l  > $1"
-sudo trace-cmd report -l  > $1
+runcmd "sudo trace-cmd report -l  > $1"
 # Typically, the report file is still fairly large (~ 9 to 10 MB in my tests)
 # as all related kernel functions show up in the trace.
--- ch9/tracecmd/trc-cmd2-mod.sh
+++ ch9/tracecmd/trc-cmd2-mod.sh
@@ -2,18 +2,21 @@
 # ch9/trace-cmd/trc-cmd1.sh
 # Simple wrapper over trace-cmd; same as trc-cmd1.sh except that here,
 # we ALSO trace only the e1000 (network driver) module!
-
 [ $# -ne 1 ] && {
   echo "Usage: $0 report-file"
   exit 1
 }
+runcmd()
+{
+    [ $# -eq 0 ] && return
+    echo "$@"
+    eval "$@"
+}
 rep=$1
 
-echo "sudo trace-cmd record -p function_graph -e net -e sock --module e1000 -F ping -c1 packtpub.com"
-sudo trace-cmd record -p function_graph -e net -e sock --module e1000 -F ping -c1 packtpub.com
+runcmd sudo trace-cmd record -p function_graph -e net -e sock --module e1000 -F ping -c1 packtpub.com
 
 [ -f $1 ] && mv -f $1 $1.bkp
-echo "sudo trace-cmd report -l  > $1"
-sudo trace-cmd report -l  > $1
+runcmd sudo trace-cmd report -l  > $1
 # Typically, the report file is now pretty tiny (~ 4 to 5 KB in my tests)
 # as ONLY the e1000 related module/kernel functions show up in the trace.
