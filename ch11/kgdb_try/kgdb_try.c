925bfc64bb28190e934357a10f13b650fc8d815b
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2022-04-29 18:32:32 +0530

    Initial commit

---
+++ ch11/kgdb_try/kgdb_try.c
@@ -0,0 +1,68 @@
+/*
+ * ch11/kgdb_try/kgdb_try.c
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Linux Kernel Debugging"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *  GitHub repository:
+ *  https://github.com/PacktPublishing/Linux-Kernel-Debugging
+ *
+ * From: Ch 11: Using Kernel GDB (KGDB)
+ ****************************************************************
+ * Brief Description:
+ *
+ * For details, please refer the book, Ch 11.
+ */
+#define pr_fmt(fmt) "%s:%s():%d: " fmt, KBUILD_MODNAME, __func__, __LINE__
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/random.h>
+#include <linux/workqueue.h>
+#include <linux/delay.h>
+#include "../../convenient.h"
+
+MODULE_AUTHOR("<insert your name here>");
+MODULE_DESCRIPTION("LKD book:ch11/kgdb_try: support for a simple demo using KGDB on a module");
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_VERSION("0.1");
+
+#define NUM  32
+u8 gdata[NUM];
+
+static void do_the_work(struct work_struct *);
+static DECLARE_DELAYED_WORK(my_work, do_the_work);
+
+/*
+ * Our delayed workqueue callback function
+ */
+static void do_the_work(struct work_struct *work)
+{
+	pr_info("In our workq function\n"); //: data=%d\n", priv->data);
+	memset(gdata-0x1000, 0xff, NUM);
+	memset(gdata, 0xff, NUM+PAGE_SIZE/2);
+	pr_info("memset() done\n");
+}
+
+static int __init kgdb_try_init(void)
+{
+	pr_info("Generating Oops via kernel bug in a delayed workqueue function\n");
+	INIT_DELAYED_WORK(&my_work, do_the_work);
+	schedule_delayed_work(&my_work, msecs_to_jiffies(2500));
+
+	return 0;		/* success */
+}
+
+static void __exit kgdb_try_exit(void)
+{
+//	memset(gdata-0x1000, 0xff, NUM);
+//	memset(gdata, 0xff, NUM+PAGE_SIZE/2);
+	cancel_delayed_work_sync(&my_work);
+	pr_info("Goodbye\n");
+}
+
+module_init(kgdb_try_init);
+module_exit(kgdb_try_exit);
